b81c1bb05341fac1a741bc0cfa78eb52e2b0fe93
jpy <1154879720@qq.com>
2022-06-30 16:55:20 +0800

    feat: :tada:

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 
 import { defineConfig } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import workerLoader from 'rollup-plugin-web-worker-loader';
-// 如果需要深度代码混淆，可开启obfuscator
-// import obfuscatorPlugin from 'rollup-plugin-javascript-obfuscator';
-
-// 参考：
-// https://github.com/SunshowerC/blog/issues/8
-
-// 命中优先级为：
-// browser > module > main
-
-// const obfuscatorOptions = {
-//   compact: true,
-//   controlFlowFlatteningThreshold: 0.75,
-//   deadCodeInjectionThreshold: 0.4,
-// };
 
 export default defineConfig({
   input: 'src/index.ts',
@@ -30,7 +14,7 @@ export default defineConfig({
     {
       file: 'dist/index.js',
       format: 'umd',
-      name: 'file-utils'
+      name: 'file-utils',
     },
   ],
   plugins: [workerLoader(), typescript(), terser()],
